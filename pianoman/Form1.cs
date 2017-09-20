9418db6357475fdfeda0706a384cf98919170c54
crazyavak <lars.boerboom@outlook.com>
2017-09-20 12:05:24 +0200

    - added libary for ev3 connection

--- pianoman/Form1.cs
+++ pianoman/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EV3MessengerLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,14 @@ namespace pianoman
 {
     public partial class Form1 : Form
     {
+
+        private EV3Messenger eV3Messenger;
+
+
         public Form1()
         {
             InitializeComponent();
+            eV3Messenger = new EV3Messenger();
         }
     }
 }
