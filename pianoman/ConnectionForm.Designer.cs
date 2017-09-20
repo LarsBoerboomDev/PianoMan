38143260c745e535a9d1f5f188d328842e18af9d
crazyavak <lars.boerboom@outlook.com>
2017-09-20 13:42:02 +0200

    - now able to connect to a port - created a Timer that sends data to the robot

--- pianoman/ConnectionForm.Designer.cs
+++ pianoman/ConnectionForm.Designer.cs
@@ -29,24 +29,95 @@
         private void InitializeComponent()
         {
             this.serialPortSelectionBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rescanPortsButton = new System.Windows.Forms.Button();
+            this.disconnectPortButton = new System.Windows.Forms.Button();
+            this.connectPortButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connectedDeviceLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // serialPortSelectionBox
             // 
             this.serialPortSelectionBox.FormattingEnabled = true;
-            this.serialPortSelectionBox.Location = new System.Drawing.Point(12, 12);
+            this.serialPortSelectionBox.Location = new System.Drawing.Point(6, 19);
             this.serialPortSelectionBox.Name = "serialPortSelectionBox";
             this.serialPortSelectionBox.Size = new System.Drawing.Size(121, 21);
             this.serialPortSelectionBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "startForm1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rescanPortsButton
+            // 
+            this.rescanPortsButton.Location = new System.Drawing.Point(133, 19);
+            this.rescanPortsButton.Name = "rescanPortsButton";
+            this.rescanPortsButton.Size = new System.Drawing.Size(75, 23);
+            this.rescanPortsButton.TabIndex = 2;
+            this.rescanPortsButton.Text = "Rescan";
+            this.rescanPortsButton.UseVisualStyleBackColor = true;
+            this.rescanPortsButton.Click += new System.EventHandler(this.rescanPortsButton_Click);
+            // 
+            // disconnectPortButton
+            // 
+            this.disconnectPortButton.Location = new System.Drawing.Point(224, 46);
+            this.disconnectPortButton.Name = "disconnectPortButton";
+            this.disconnectPortButton.Size = new System.Drawing.Size(75, 23);
+            this.disconnectPortButton.TabIndex = 3;
+            this.disconnectPortButton.Text = "Disconnect";
+            this.disconnectPortButton.UseVisualStyleBackColor = true;
+            this.disconnectPortButton.Click += new System.EventHandler(this.disconnectPortButton_Click);
+            // 
+            // connectPortButton
+            // 
+            this.connectPortButton.Location = new System.Drawing.Point(224, 17);
+            this.connectPortButton.Name = "connectPortButton";
+            this.connectPortButton.Size = new System.Drawing.Size(75, 23);
+            this.connectPortButton.TabIndex = 4;
+            this.connectPortButton.Text = "Connect";
+            this.connectPortButton.UseVisualStyleBackColor = true;
+            this.connectPortButton.Click += new System.EventHandler(this.connectPortButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.connectedDeviceLabel);
+            this.groupBox1.Controls.Add(this.serialPortSelectionBox);
+            this.groupBox1.Controls.Add(this.disconnectPortButton);
+            this.groupBox1.Controls.Add(this.connectPortButton);
+            this.groupBox1.Controls.Add(this.rescanPortsButton);
+            this.groupBox1.Location = new System.Drawing.Point(37, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "port Connection";
+            // 
+            // connectedDeviceLabel
+            // 
+            this.connectedDeviceLabel.Location = new System.Drawing.Point(305, 22);
+            this.connectedDeviceLabel.Name = "connectedDeviceLabel";
+            this.connectedDeviceLabel.Size = new System.Drawing.Size(97, 42);
+            this.connectedDeviceLabel.TabIndex = 5;
+            this.connectedDeviceLabel.Text = "label1";
             // 
             // ConnectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.serialPortSelectionBox);
+            this.ClientSize = new System.Drawing.Size(692, 343);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
             this.Name = "ConnectionForm";
             this.Text = "ConnectionForm";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -54,5 +125,11 @@
         #endregion
 
         private System.Windows.Forms.ComboBox serialPortSelectionBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button rescanPortsButton;
+        private System.Windows.Forms.Button disconnectPortButton;
+        private System.Windows.Forms.Button connectPortButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label connectedDeviceLabel;
     }
 }
--- pianoman/ConnectionForm.cs
+++ pianoman/ConnectionForm.cs
@@ -15,10 +15,18 @@ namespace pianoman
     public partial class ConnectionForm : Form
     {
         private EV3Messenger eV3Messenger;
+
+        private Timer messageReceiverTimer;
         public ConnectionForm()
         {
             InitializeComponent();
             eV3Messenger = new EV3Messenger();
+
+            //Starts timer 
+            messageReceiverTimer = new Timer();
+            messageReceiverTimer.Interval = 10;
+            messageReceiverTimer.Tick += new System.EventHandler(messageReadTimer_Tick);
+
             FillSerialPortSelectionBoxWithAvailablePorts();
         }
         private void FillSerialPortSelectionBoxWithAvailablePorts()
@@ -30,6 +38,61 @@ namespace pianoman
             {
                 serialPortSelectionBox.Items.Add(port);
             }
+        }
+
+
+
+        //just a test button to open form1 to be changed
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.Show();
+        }
+
+        //rescans the ports
+
+        private void rescanPortsButton_Click(object sender, EventArgs e)
+        {
+            FillSerialPortSelectionBoxWithAvailablePorts();
+        }
+
+        private void connectPortButton_Click(object sender, EventArgs e)
+        {
+            String port = serialPortSelectionBox.Text;
+            if (eV3Messenger.Connect(port))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Failed to connect to port");
+            }
+        }
+        private void messageReadTimer_Tick(object sender, EventArgs e)
+        {
+           
+        }
+        private void UpdateButtonsAndConnectionInfo()
+        {
+            bool isConnected = eV3Messenger.IsConnected;
+            connectPortButton.Enabled = !isConnected;
+
+            if (isConnected)
+            {
+                connectedDeviceLabel.Text = "Connected to: " + eV3Messenger.Port;
+                connectedDeviceLabel.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                connectedDeviceLabel.Text = "No connection";
+                connectedDeviceLabel.BackColor = Color.Red;
+            }
+        }
+
+        private void disconnectPortButton_Click(object sender, EventArgs e)
+        {
+            eV3Messenger.Disconnect();
+            UpdateButtonsAndConnectionInfo();
         }
     }
 }
