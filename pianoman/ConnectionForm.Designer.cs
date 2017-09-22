294c38ee76eacf2c1ecf1b438ab72ad768be9afc
crazyavak <lars.boerboom@outlook.com>
2017-09-22 11:42:05 +0200

    - now fills the listbox with notes and intervals

--- pianoman/ConnectionForm.Designer.cs
+++ pianoman/ConnectionForm.Designer.cs
@@ -52,7 +52,6 @@
             // 
             // button1
             // 
-            this.button1.Enabled = false;
             this.button1.Location = new System.Drawing.Point(370, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
--- pianoman/Form1.cs
+++ pianoman/Form1.cs
@@ -12,25 +12,32 @@ namespace pianoman
 {
     public partial class Form1 : Form
     {
+
+        public List<string> noteList = new List<string>();
+        //private Dictionary<string, decimal> noteList = new Dictionary<string, decimal>();
         public Form1()
         {
             InitializeComponent();
         }
-
-        string noot;
-        decimal time;
-
+        
         private void addnoteButton_Click(object sender, EventArgs e)
         {
-            noot = nootCombo.Text;
-            time = timeNummeric.Value;
+           string noot = nootCombo.Text;
+           decimal time = timeNummeric.Value;
+           string note = noot + "," + time;
 
-            List<string>[] list = new List<String>[];
-            list[0] = new List<string>();
-            list[1] = new List<string>();
-
-            list[0].add("hello");
-            list[1].add("world");
+           noteList.Add(note);
+           fillMusicList();                            
         }
+
+        private void fillMusicList()
+        {
+            listBox1.Items.Clear();
+           foreach(string notes in noteList)
+            {
+                listBox1.Items.Add(notes);
+            }
+        }
+
     }
 }
